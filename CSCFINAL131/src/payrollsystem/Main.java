738ad4df13bc99af9e1aaa32ab1dc7b1a59b457a
Jimmy Boi <Jimmy Boi@DESKTOP-JQIJFC5>
2025-04-28 22:37:23 -0700

    Commit two: added my ID

--- CSCFINAL131/src/payrollsystem/Main.java
+++ CSCFINAL131/src/payrollsystem/Main.java
@@ -5,6 +5,9 @@ public class Main {
 		Employee emp1 = new Employee("Colin", "Nugent", 7999, 30f,0f, 17f);
 		System.out.println(emp1.getFirstName());
 		
+		Employee emp2 = new Employee("Jimson", "Sou", 8000, 35f,2f, 25f);
+		System.out.println(emp2.getFirstName());
+		
 	}
 
 }
