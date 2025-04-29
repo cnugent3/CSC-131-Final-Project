71c0e3a33ad651f698162ffe2b857fda919dcb70
jumanaabdullah <jumanaabdullah@localhost>
2025-04-29 15:05:12 -0700

    added new employee

--- CSCFINAL131/src/payrollsystem/Main.java
+++ CSCFINAL131/src/payrollsystem/Main.java
@@ -3,6 +3,7 @@ package payrollsystem;
 public class Main {
 	public static void main(String[] args) {
 		Employee emp1 = new Employee("Colin", "Nugent", 7999, 30f,0f, 17f);
+		Employee emp2 = new Employee("Abdullah", "Jumana", 2345, 12, 12, 13);
 		System.out.println(emp1.getFirstName());
 		
 	}
