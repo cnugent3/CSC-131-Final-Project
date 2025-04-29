4f844b78ffa39b9baf74f0c49b91cdecd343d185
evanr <evanr@DESKTOP-NSIDNRI>
2025-04-28 22:40:15 -0700

    commit 3: added my id

--- CSCFINAL131/src/payrollsystem/Main.java
+++ CSCFINAL131/src/payrollsystem/Main.java
@@ -2,10 +2,13 @@ package payrollsystem;
 
 public class Main {
 	public static void main(String[] args) {
-		Employee emp1 = new Employee("Colin", "Nugent", 7999, 30f,0f, 17f);
+		Employee emp1 = new Employee("Colin", "Nugent", 7999, 30f, 0f, 17f);
 		System.out.println(emp1.getFirstName());
 		
-		Employee emp2 = new Employee("Jimson", "Sou", 8000, 35f,2f, 25f);
+		Employee emp2 = new Employee("Jimson", "Sou", 8000, 35f, 2f, 25f);
+		System.out.println(emp2.getFirstName());
+		
+		Employee emp3 = new Employee("Evan", "Zheng", 8001, 40f, 4f, 30f);
 		System.out.println(emp2.getFirstName());
 		
 	}
